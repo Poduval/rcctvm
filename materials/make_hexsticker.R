9a88db623aad9430ab1abb8a848fd3589b19e9fd
Rakesh Poduval <poduval.rakesh@icloud.com>
2025-05-06 23:04:23 +0530

    update path to the logo

--- materials/make_hexsticker.R
+++ materials/make_hexsticker.R
@@ -2,7 +2,7 @@ library(hexSticker)
 library(ggplot2)
 
 # Generate sticker
-sticker("man/figures/logo.png",
+sticker("materials/logo.png",
         package = "rcctvm",
         p_size = 20, p_y = 1.65,
         s_x = 1, s_y = 0.95, s_width = 0.6,   # Adjust position/size inside hex
